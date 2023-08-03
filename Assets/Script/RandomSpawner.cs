b5d42b957e205bc8faa258341717e2461fb79ea2
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-03 21:38:45 +0800

    Random Spawner

--- Assets/Script/RandomSpawner.cs
+++ Assets/Script/RandomSpawner.cs
@@ -2,71 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomSpawner : MonoBehaviour
+
+namespace CollectionSystem
 {
-    public GameObject ItemOne;
-
-    public GameObject ItemTwo;
-    //public float Radius = 1;
-    int ItemsAtSpawn = 1;
-
-
-    private void Start()
+    public class RandomSpawner : MonoBehaviour
     {
-        SpawnItemOne();
-        SpawnItemTwo();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        /*if(Input.GetKeyDown(KeyCode.Space))
+        public GameObject ItemOne;
+        public GameObject ItemTwo;
+        int ItemsAtSpawn = 2;
+
+        private void Start()
         {
-
             SpawnItemOne();
             SpawnItemTwo();
-        }*/
-    }
+        }
 
-    public void SpawnItemOne()
-    {
-        /*int SpawnPointX = Random.Range(0, 11);
-        int SpawnPointZ = Random.Range(0, 11);
-        int SpawnPointY = Random.Range(0, 11);
-
-        Vector3 SpawnPosition = new Vector3(SpawnPointX, SpawnPointY, SpawnPointZ);*/
-
-        for (var i = 0; i <= ItemsAtSpawn; i++)
+        public void Message()
         {
-            Vector3 SpawnPosition = new Vector3(Random.Range(-10, 11), 1, Random.Range(-10, 11));
+            Debug.Log("This is Random Spawner");
+        }
 
-            Instantiate(ItemOne, SpawnPosition, Quaternion.identity);
+        public void SpawnItemOne()
+        {
 
-            Debug.Log(i);
+            /*int SpawnPointX = Random.Range(0, 11);
+            int SpawnPointZ = Random.Range(0, 11);
+            int SpawnPointY = Random.Range(0, 11);
+
+            Vector3 SpawnPosition = new Vector3(SpawnPointX, SpawnPointY, SpawnPointZ);*/
+
+            for (var NumberOfItems = 0; NumberOfItems <= ItemsAtSpawn; NumberOfItems++)
+            {
+                Vector3 SpawnPosition = new Vector3(Random.Range(-10, 11), 1, Random.Range(-10, 11));
+
+                Instantiate(ItemOne, SpawnPosition, Quaternion.identity);
+
+                Debug.Log("Number Of Item Ones is " + NumberOfItems);
+            }
+        }
+
+        public void SpawnItemTwo()
+        {
+
+            for (var i = 0; i <= ItemsAtSpawn; i++)
+            {
+                Vector3 SpawnPosition = new Vector3(Random.Range(-10, 11), 1, Random.Range(-10, 11));
+
+                Instantiate(ItemTwo, SpawnPosition, Quaternion.identity);
+
+                Debug.Log(i);
+            }
         }
     }
-
-    public void SpawnItemTwo()
-    {
-        /*int SpawnPointX = Random.Range(0, 11);
-        int SpawnPointZ = Random.Range(0, 11);
-        int SpawnPointY = Random.Range(0, 11);
-
-        Vector3 SpawnPosition = new Vector3(SpawnPointX, SpawnPointY, SpawnPointZ);*/
-
-        for (var i = 0; i <= ItemsAtSpawn; i++)
-        {
-            Vector3 SpawnPosition = new Vector3(Random.Range(-10, 11), 1, Random.Range(-10, 11));
-
-            Instantiate(ItemTwo, SpawnPosition, Quaternion.identity);
-
-            Debug.Log(i);
-        }
-    }
-
-    /*private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-
-        Gizmos.DrawWireSphere(this.transform.position, Radius);
-    }*/
 }
