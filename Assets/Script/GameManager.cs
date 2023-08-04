deabed9ce02c2b0db723f544bdf4d8ab895d1876
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-05 03:46:43 +0800

    Game Manager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class GameManager : MonoBehaviour
     public GameObject QuitMenu;
 
     public GameObject PauseMenu;
+
+    public TextMeshProUGUI ItemsCollected;
+
+    public TextMeshProUGUI NumberOfItems;
 
     private void Start()
     {
