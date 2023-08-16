302beb7f234ae1dd5dd03608eed1bd72ff3ed786
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-16 14:57:08 +0800

    player controller

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -23,6 +23,8 @@ namespace CollectionSystem
 
         public bool canMove = true;
 
+        public bool PlayerMovement = true;
+
         CharacterController characterController;
         // Start is called before the first frame update
         void Start()
@@ -37,52 +39,48 @@ namespace CollectionSystem
         // Update is called once per frame
         void Update()
         {
-
-            /*if (Input.GetKeyDown(KeyCode.R))
+            if (PlayerMovement)
             {
-                Test += 1;
-                Debug.Log("This is Test " + Test);
-            }*/
+                //Movement
+                Vector3 forward = transform.TransformDirection(Vector3.forward);
+                Vector3 right = transform.TransformDirection(Vector3.right);
 
-            //Movement
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-            Vector3 right = transform.TransformDirection(Vector3.right);
+                bool isRunning = Input.GetKey(KeyCode.LeftShift);
+                float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+                float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+                float movementDirectionY = moveDirection.y;
+                moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-            bool isRunning = Input.GetKey(KeyCode.LeftShift);
-            float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-            float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-            float movementDirectionY = moveDirection.y;
-            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+                //Jumping
+                if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+                {
+                    moveDirection.y = jumpPower;
+                }
+                else
+                {
+                    moveDirection.y = movementDirectionY;
+                }
+                if (!characterController.isGrounded)
+                {
+                    moveDirection.y -= gravity * Time.deltaTime;
+                }
+                //Rotation
+                characterController.Move(moveDirection * Time.deltaTime);
 
-            //Jumping
-            if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-            {
-                moveDirection.y = jumpPower;
+                if (canMove)
+                {
+                    rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+                    rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+                    playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+                    transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+                }
+
+                /*if (Input.GetKey(KeyCode.Escape))
+                {
+                    Debug.Log("Quit");
+                    Application.Quit();
+                }*/
             }
-            else
-            {
-                moveDirection.y = movementDirectionY;
-            }
-            if (!characterController.isGrounded)
-            {
-                moveDirection.y -= gravity * Time.deltaTime;
-            }
-            //Rotation
-            characterController.Move(moveDirection * Time.deltaTime);
-
-            if (canMove)
-            {
-                rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-                rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-                playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-                transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-            }
-
-            /*if (Input.GetKey(KeyCode.Escape))
-            {
-                Debug.Log("Quit");
-                Application.Quit();
-            }*/
         }
     }
 }
