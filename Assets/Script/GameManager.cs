a731518368ade871521f962d2b72deae82d01b54
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-18 00:37:12 +0800

    game manager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,49 +4,63 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+namespace CollectionSystem
 {
-    public GameObject MainMenu;
-
-    public GameObject OptionsMenu;
-
-    public GameObject HowToPlayMenu;
-
-    public GameObject QuitMenu;
-
-    public GameObject PauseMenu;
-
-    public GameObject CreditsMenu;
-
-   
-    private void Start()
+    public class GameManager : MonoBehaviour
     {
-        QuitMenu.SetActive(false);
-        OptionsMenu.SetActive(false);
-        HowToPlayMenu.SetActive(false);
-        CreditsMenu.SetActive(false);
-        MainMenu.SetActive(true);
-        FindObjectOfType<AudioManager>().Play("Theme");
-    }
-    public void OnStartButton()
-    {
-        SceneManager.LoadScene(1);
-        FindObjectOfType<AudioManager>().StopPlaying("Theme");
+        public GameObject MainMenu;
 
-    }
+        public GameObject OptionsMenu;
 
-    public void OnYesButton()
-    {
-        Application.Quit();
-    }
+        public GameObject HowToPlayMenu;
 
-    public void SetQuality (int qualityIndex)
-    {
-        QualitySettings.SetQualityLevel(qualityIndex);
-    }
+        public GameObject QuitMenu;
 
-    public void OnButton()
-    {
-        FindObjectOfType<AudioManager>().Play("button Sound");
+        public GameObject PauseMenu;
+
+        public GameObject CreditsMenu;
+
+
+        private void Start()
+        {
+            QuitMenu.SetActive(false);
+            OptionsMenu.SetActive(false);
+            HowToPlayMenu.SetActive(false);
+            CreditsMenu.SetActive(false);
+            MainMenu.SetActive(true);
+            FindObjectOfType<AudioManager>().Play("Theme");
+        }
+        public void OnStartButton()
+        {
+            SceneManager.LoadScene(1);
+            FindObjectOfType<AudioManager>().StopPlaying("Theme");
+
+        }
+
+        public void OnYesButton()
+        {
+            Application.Quit();
+        }
+
+        public void SetQuality(int qualityIndex)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+        }
+
+        public void OnButton()
+        {
+            FindObjectOfType<AudioManager>().Play("button Sound");
+        }
+        public void OnQuitButton()
+        {
+
+        }
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Backspace))
+            {
+                PauseMenu.SetActive(true);
+            }
+        }
     }
 }
