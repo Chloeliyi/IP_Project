a887e3e14bfa0aeb4ee1feaa7106a9f090131143
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-18 22:29:13 +0800

    inventory ui

--- Assets/Script/InventoryUI.cs
+++ Assets/Script/InventoryUI.cs
@@ -10,19 +10,27 @@ namespace CollectionSystem
     public class InventoryUI : MonoBehaviour
     {
 
-        private TextMeshProUGUI ItemsCollectedLabel;
+        public TextMeshProUGUI ItemsCollectedLabel;
+        private TextMeshProUGUI ItemsForOrangeCollectedLabel;
         //public TextMeshProUGUI PartsLabel;
 
         void Start()
         {
             ItemsCollectedLabel = GetComponent<TextMeshProUGUI>();
-          //PartsLabel = GetComponent<TextMeshProUGUI>();
+            ItemsForOrangeCollectedLabel = GetComponent<TextMeshProUGUI>();
+            //PartsLabel = GetComponent<TextMeshProUGUI>();
         }
 
         public void UpdateItemLabel(Inventory Inventory)
         {
             ItemsCollectedLabel.text = Inventory.NumberOfItemsCollected.ToString();
           //PartsLabel.text = "Parts: " + Inventory.NumberOfItemsCollected.ToString();
+        }
+
+        public void UpdateItemForOrangeLabel(Inventory Inventory)
+        {
+            ItemsForOrangeCollectedLabel.text = Inventory.NumberOfItemsForOrangeCollected.ToString();
+            //PartsLabel.text = "Parts: " + Inventory.NumberOfItemsCollected.ToString();
         }
     }
 }
