384926d1ac0c4330c6aef1c116608756b919b72a
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-19 18:05:24 +0800

    door animation

--- Assets/Script/PlayRacingDoors.cs
+++ Assets/Script/PlayRacingDoors.cs
@@ -2,21 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //Chloe Chan
-public class PlaySlideDoors : MonoBehaviour
+public class PlayRacingDoors : MonoBehaviour
 {
-    //Play slide door animation
-    public Animator SlidedoorAnim;
-
+    //Play racing door animation
+    public Animator RacedoorAnim;
     [Header("Animation Names")]
-    [SerializeField] private string openAnimationName = "SlideDoorsOpen";
+    [SerializeField] private string openAnimationName = "RacingDoorsOpen";
 
     [SerializeField] private int waitTimer = 3;
 
     [SerializeField] private bool PlayOnce = false;
-
     private void Awake()
     {
-        SlidedoorAnim = gameObject.GetComponent<Animator>();
+        RacedoorAnim = gameObject.GetComponent<Animator>();
     }
 
     void OnTriggerEnter(Collider coll)
@@ -26,7 +24,7 @@ public class PlaySlideDoors : MonoBehaviour
             Debug.Log("Player is in range of Door");
             if (!PlayOnce)
             {
-                SlidedoorAnim.Play(openAnimationName, 0, 0.0f);
+                RacedoorAnim.Play(openAnimationName, 0, 0.0f);
                 Debug.Log("Door Is Open");
                 PlayOnce = true;
                 Debug.Log("PlayOnce is : " + PlayOnce);
@@ -38,7 +36,7 @@ public class PlaySlideDoors : MonoBehaviour
     {
         //pauseInteraction = true;
         yield return new WaitForSeconds(waitTimer);
-        SlidedoorAnim.SetTrigger("Start");
+        RacedoorAnim.SetTrigger("Start");
         Debug.Log("Door Is Closed");
         PlayOnce = false;
         Debug.Log("PlayOnce is : " + PlayOnce);
