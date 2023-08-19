c402b6e8474971cbd3eefe0fa7ce758627a3efcc
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-19 18:04:44 +0800

    billboard

--- Assets/Script/Billboard.cs
+++ Assets/Script/Billboard.cs
@@ -1,9 +1,17 @@
+/*
+ * Author: Chloe Chan
+ * Date: 8/19/2023
+ * Description: The billboard for making the object look at player camera
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    /// <summary>
+    /// Camera transform
+    /// </summary>
     public Transform Camera;
 
     void LateUpdate()
