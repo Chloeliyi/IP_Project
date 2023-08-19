4aa8ecd7a0e0602651c8d62611c0bd12c2ce29db
Chloeliyi <s10247578@connect.np.edu.sg>
2023-08-19 16:36:02 +0800

    play slide door

---
+++ Assets/Script/PlaySlideDoors.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySlideDoors : MonoBehaviour
+{
+    public Animator SlidedoorAnim;
+
+    [Header("Animation Names")]
+    [SerializeField] private string openAnimationName = "SlideDoorsOpen";
+
+    [SerializeField] private int waitTimer = 3;
+
+    [SerializeField] private bool PlayOnce = false;
+
+    private void Awake()
+    {
+        SlidedoorAnim = gameObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "Player" || coll.tag == "NPC")
+        {
+            Debug.Log("Player is in range of Door");
+            if (!PlayOnce)
+            {
+                SlidedoorAnim.Play(openAnimationName, 0, 0.0f);
+                Debug.Log("Door Is Open");
+                PlayOnce = true;
+                Debug.Log("PlayOnce is : " + PlayOnce);
+                StartCoroutine(PauseDoorInteraction());
+            }
+        }
+    }
+    private IEnumerator PauseDoorInteraction()
+    {
+        //pauseInteraction = true;
+        yield return new WaitForSeconds(waitTimer);
+        SlidedoorAnim.SetTrigger("Start");
+        Debug.Log("Door Is Closed");
+        PlayOnce = false;
+        Debug.Log("PlayOnce is : " + PlayOnce);
+        //pauseInteraction = false;
+    }
+}
